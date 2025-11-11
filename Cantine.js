edacb3bd6999c22b1f80d38f9feef17c15758d7a
kohotatsu <158819634+kohotatsu@users.noreply.github.com>
2025-11-12 00:31:37 +0100

    tinkering with canvas size

--- Cantine.js
+++ Cantine.js
@@ -35,15 +35,10 @@ class Card {
     }
     Init(){
 
-        //this.blackCanvasContext.font = "20pt Roboto Condensed";
-        //this.colorCanvasContext.font = "20pt Roboto Condensed";
-
-        //this.blackCanvasContext.lineWidth = 7;
-        //this.colorCanvasContext.lineWidth = 7;
         this.blackCanvasContext.strokeStyle = "#EBECEC";
         this.colorCanvasContext.strokeStyle = "#D2DDF1";
 
-        this.blackBackground_img.src = "img/NB/Fonds/Fond.png";
+        this.blackBackground_img.src = "img/NB/Fonds/Fond.svg";
         this.blackBackground_img.onload = function (){
             initLoaded();
         }
@@ -129,44 +124,35 @@ class Card {
 
     UpdateCanvas(){
         console.log("Update");
-        this.blackCanvasContext.drawImage(this.blackProfileSource, 22, 52,88,88);
-        this.colorCanvasContext.drawImage(this.colorProfileSource, 22, 52,88,88);
+        this.blackCanvasContext.drawImage(this.blackProfileSource, 61, 142,240,240);
+        this.colorCanvasContext.drawImage(this.colorProfileSource, 61, 142,240,240);
 
-        this.blackCanvasContext.drawImage(this.blackBackground_img,0,0,380,242);
-        this.colorCanvasContext.drawImage(this.colorBackground_img,0,0,380,242);
+        this.blackCanvasContext.drawImage(this.blackBackground_img,0,0,canvasWidth,canvasHeight);
+        this.colorCanvasContext.drawImage(this.colorBackground_img,0,0,canvasWidth,canvasHeight);
 
         //-STATUT-
         let fullStatut = this.statut;
         if(/^I/.test(this.statut) && this.inviteur != ""){
             fullStatut += " de " + this.inviteur ;
         }
-        this.PrintOnCard(fullStatut.toUpperCase(),122,90);
-        this.PrintOnCard("Statut:",122,90-25,true);
-
-        //this.blackCanvasContext.drawImage(this.blackTitleStatut_img,0,0,380,242);
-        //this.colorCanvasContext.drawImage(this.colorTitleStatut_img,0,0,380,242);
+        this.PrintOnCard(fullStatut.toUpperCase(),320,240);
+        this.PrintOnCard("Statut:",320,240-25,true);
 
         //-PSEUDO--
-        this.PrintOnCard(this.pseudo.toUpperCase(),122,135);
-        this.PrintOnCard("Pseudo:",122,135-25,true);
-        //this.blackCanvasContext.drawImage(this.blackTitlePseudo_img,0,0,380,242);
-        //this.colorCanvasContext.drawImage(this.colorTitlePseudo_img,0,0,380,242);
+        this.PrintOnCard(this.pseudo.toUpperCase(),320,330);
+        this.PrintOnCard("Pseudo:",320,330-25,true);
 
         //-ALIAS-
-        this.PrintOnCard(this.alias.toUpperCase(),122,185);
-        this.PrintOnCard("Prénom/Alias:",122,185-25,true);
-        //this.blackCanvasContext.drawImage(this.blackTitleAlias_img,0,0,380,242);
-        //this.colorCanvasContext.drawImage(this.colorTitleAlias_img,0,0,380,242);
+        this.PrintOnCard(this.alias.toUpperCase(),320,410);
+        this.PrintOnCard("Prénom/Alias:",320,410-25,true);
 
         //-PRONOM-
-        this.PrintOnCard(this.pronoms.toUpperCase().replaceAll(" "," / "),120,230);
-        this.PrintOnCard("Pronoms:",122,230-25,true);
-        //this.blackCanvasContext.drawImage(this.blackTitlePronoms_img,0,0,380,242);
-        //this.colorCanvasContext.drawImage(this.colorTitlePronoms_img,0,0,380,242);
+        this.PrintOnCard(this.pronoms.toUpperCase().replaceAll(" "," / "),320,520);
+        this.PrintOnCard("Pronoms:",320,520-25,true);
 
         //-HOUSE-
-        this.blackCanvasContext.drawImage(this.blackHouseImg,0,0);
-        this.colorCanvasContext.drawImage(this.colorHouseImg,0,0);
+        this.blackCanvasContext.drawImage(this.blackHouseImg,0,0,canvasWidth,canvasHeight);
+        this.colorCanvasContext.drawImage(this.colorHouseImg,0,0,canvasWidth,canvasHeight);
     }
     PrintOnCard(text,xPos,yPos,label = false){
         if(label){
@@ -194,11 +180,12 @@ class Card {
 
 function DownloadCard(type) {
     let cardImage = type == 0?
-        colorCanvas.toDataURL("cardImage/png"):
-        blackCanvas.toDataURL("cardImage/png");
+        blackCanvas.toDataURL("cardImage_black/png",1):
+        colorCanvas.toDataURL("cardImage_color/png",1);
     let aDownloadLink = document.createElement('a');
     // Add the name of the file to the link
-    aDownloadLink.download = 'canvas_image.png';
+    let colorString = type == 0?"Black_":"Color_";
+    aDownloadLink.download = colorString+'canvas_image.png';
     // Attach the data to the link
     aDownloadLink.href = cardImage;
     // Get the code to click the download link
@@ -218,23 +205,19 @@ function loadFont(name,url){
     });
 }
 loadFont("Roboto Condensed","https://fonts.googleapis.com/css2?family=Pixelify+Sans:wght@400..700&family=Roboto+Condensed&display=swap")
-/*
-let customFont = new FontFace("Roboto Condensed", "url(" + "https://fonts.googleapis.com/css2?family=Pixelify+Sans:wght@400..700&family=Roboto+Condensed&display=swap" + ")");
 
-customFont.load().then((font) => {
-    document.fonts.add(font);
-    console.log("Font loaded: "+"Roboto Condensed");
-});
-*/
+let colorCanvas = document.getElementById("color-canvas");
+let blackCanvas = document.getElementById("black-canvas");
 
-let colorCanvas = document.getElementById("black-canvas");
-let blackCanvas = document.getElementById("color-canvas");
+const canvasWidth = blackCanvas.width;
+const canvasHeight = blackCanvas.height;
+
 let colorCanvas_context = colorCanvas.getContext("2d");
 let blackCanvas_context = blackCanvas.getContext("2d");
 
 let initLoadCnt = 0;
 //init();
-card = new Card(colorCanvas_context, blackCanvas_context);
+card = new Card(blackCanvas_context,colorCanvas_context);
 card.Init();
 
 //Events---------------
@@ -246,7 +229,6 @@ document.getElementById('statut').onchange = function () {
         useDisplay = "block";
     }
     document.getElementById("inviteur-item").style.display = useDisplay;
-    //document.getElementById("inviteur-label").style.display = useDisplay;
 
     card.UpdateCanvas();
 }
@@ -280,8 +262,6 @@ document.getElementById('profilImage').onchange = function () {
     card.colorProfileSource.onload = function (){
         reloadCanvas();
     }
-    //card.UpdateCanvas();
 }
 
 card.UpdateCanvas();
-//card.SetBackgroundImage("img/Fond_Couleur.png");
